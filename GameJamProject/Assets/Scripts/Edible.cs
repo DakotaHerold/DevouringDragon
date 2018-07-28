f20aa40159968f966c01e223a29bd6b3da63988f
jjames379 <jjames@essexsoft.com>
2018-07-28 15:37:57 -0400

    Fixed Edible to only score when player eats and removed events

--- GameJamProject/Assets/Scripts/Edible.cs
+++ GameJamProject/Assets/Scripts/Edible.cs
@@ -5,21 +5,13 @@ using UnityEngine;
 public class Edible : MonoBehaviour {
     public Collider bounds;
     public int points;
-
-    public delegate void EventAction();
-    public static event EventAction OnEdibleDetroyed;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         float dist = GameHandler.Instance.playerSpeed * Time.deltaTime;
         this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - dist);
-        if (this.gameObject.transform.position.y < -20) EdibleDestroyed();
+        if (this.gameObject.transform.position.y < -20) EdibleDestroyed(false);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -27,11 +19,10 @@ public class Edible : MonoBehaviour {
         EdibleDestroyed();
     }
 
-    public void EdibleDestroyed()
+    public void EdibleDestroyed(bool byPlayer = true)
     {
         // Here we want to eat the edible
-        GameHandler.Instance.score += points;
-        //OnEdibleDetroyed();
+        if(byPlayer) GameHandler.Instance.score += points;
         Destroy(gameObject, 0.2f);
     }
 }
