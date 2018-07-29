88aaf1e56ab0b0ba4fbc038c437585e0fe4bc22e
jjames379 <jjames@essexsoft.com>
2018-07-29 15:20:45 -0400

    Updated Background

--- GameJamProject/Assets/Scripts/Ground.cs
+++ GameJamProject/Assets/Scripts/Ground.cs
@@ -7,6 +7,7 @@ public class Ground : MonoBehaviour
     public Transform scroll1;
     public Transform scroll2;
     public Transform scroll3;
+    //private float spriteHeight = 34.38f;
 
 	void Update ()
     {
@@ -14,9 +15,12 @@ public class Ground : MonoBehaviour
         {
             float dist = GameHandler.Instance.playerSpeed * Time.deltaTime;
             GameHandler.Instance.DistanceTravelled += dist;
-            scroll1.position = new Vector3(scroll1.position.x, scroll1.position.y - dist + ((scroll1.position.y - dist < -30f) ? 64.5f : 0f));
-            scroll2.position = new Vector3(scroll2.position.x, scroll2.position.y - dist + ((scroll2.position.y - dist < -30f) ? 64.5f : 0f));
-            scroll3.position = new Vector3(scroll3.position.x, scroll3.position.y - dist + ((scroll3.position.y - dist < -30f) ? 64.5f : 0f));
+            //scroll1.position = new Vector3(scroll1.position.x, scroll1.position.y - dist + ((scroll1.position.y - dist < -30f) ? (3f * spriteHeight) : 0f));
+            //scroll2.position = new Vector3(scroll2.position.x, scroll2.position.y - dist + ((scroll2.position.y - dist < -30f) ? (3f * spriteHeight) : 0f));
+            //scroll3.position = new Vector3(scroll3.position.x, scroll3.position.y - dist + ((scroll3.position.y - dist < -30f) ? (3f * spriteHeight) : 0f));
+            scroll1.position = new Vector3(scroll1.position.x, scroll1.position.y - dist + ((scroll1.position.y - dist < -30f) ? 51f : 0f));
+            scroll2.position = new Vector3(scroll2.position.x, scroll2.position.y - dist + ((scroll2.position.y - dist < -30f) ? 51f : 0f));
+            scroll3.position = new Vector3(scroll3.position.x, scroll3.position.y - dist + ((scroll3.position.y - dist < -30f) ? 51f : 0f));
         }
-	}
+    }
 }
